b5b892106fc2b515fe5196dd26515147f711d42c
Ben Darlow <github@kapowaz.net>
2011-02-01 13:48:56 +0000

    Added JSON gem to support (partial) implementation of vendor prices for items, added a static list of realms (for now), added a preferences UI at the top of each page (mostly non-functional for now).

--- application.rb
+++ application.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "open-uri"
 require "nokogiri"
+require "json"
 require "merbhelpers"
 require "helpers"
 
@@ -10,6 +11,33 @@ include Helpers
 get "/" do
   @page = { :title => "wowcrafter" }
   erb :index
+end
+
+post "/" do
+  # TODO: optimise. This is all horrendously slow currently
+  # due to the fact it performs two round-trips to wowhead's 
+  # item XML for any single request, even before it looks up 
+  # reagent prices
+  
+  recipe_from_id = recipe_by_id(params[:query])
+  
+  if recipe_from_id[:error].nil?
+    redirect "/id/#{params[:query]}"
+  else
+    item_name = URI.escape(params[:query], Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    recipe_from_name = recipe_by_id(item_id_from_name(params[:query]))
+    if recipe_from_name[:error].nil?
+      redirect "/item/#{item_name}"
+      erb :notfound
+    else
+      # wasn't either an item ID or a direct name match; instead show a list of search results...
+      erb :notfound
+    end
+  end
+end
+
+get "/realms" do
+  erb :realms, :layout => false
 end
 
 get "/id/:item_id" do |item_id|
@@ -22,12 +50,13 @@ get "/id/:item_id" do |item_id|
 end
 
 get "/item/:item_name" do |item_name|
-  recipe = item_id_from_name(recipe_by_id(item_name))
-  if recipe[:error].nil?
-    title = "wowcrafter — Recipe for #{recipe[:name]}"
-  end
-  @page = { :title => title, :recipe => recipe }
-  erb :item
+  # TODO: implement item_id_from_name to make this work...
+  # recipe = item_id_from_name(recipe_by_id(item_name))
+  # if recipe[:error].nil?
+  #   title = "wowcrafter — Recipe for #{recipe[:name]}"
+  # end
+  # @page = { :title => title, :recipe => recipe }
+  erb :notfound
 end
 
 error 403 do
