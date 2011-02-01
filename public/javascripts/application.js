c52b3ef551d36ddb6b0e5e08c6b48cca52c0f60f
Ben Darlow <github@kapowaz.net>
2011-02-01 13:48:56 +0000

    Added JSON gem to support (partial) implementation of vendor prices for items, added a static list of realms (for now), added a preferences UI at the top of each page (mostly non-functional for now).

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,13 +30,37 @@ $(document).ready(function() {
 	});
 	
 	var resizeWrapper = function(){
-    var window_height = Math.round($(window).height()) - 10;
+    var window_height = Math.round($(window).height());
     $('div#wrapper').css({'height': window_height});
 	};
 	
   resizeWrapper();
 	$(window).bind('resize', resizeWrapper);
-
+  $(document).bind('scroll', resizeWrapper);
+  
   $('button, a.button').buttonWrapper();
-
+  
+  // initially disable the selects for faction and server
+  $('input#use_locale').bind('click', function(){
+    $(this).blur();
+  });
+  
+  $('select[name=faction], select[name=realm]').attr('disabled', 'disabled');
+  
+  $('input#use_locale').bind('change', function(){
+    if ($(this).is(':checked')) {
+      $('select[name=faction], select[name=realm]').removeAttr('disabled');
+    } else {
+      $('select[name=faction], select[name=realm]').val('average');
+      $('select[name=faction], select[name=realm]').attr('disabled', 'disabled');
+    }
+    // TODO: post the form so as to update settings — important: needs to know which page you were on when you changed your locale settings
+  });
+  
+  // post the form with locale settings whenever the dropdowns change value
+  var updateLocaleSettings = function(){
+    // TODO: implementation...
+  };
+  $('select[name=faction], select[name=select]').bind('change', updateLocaleSettings);
+  
 });
