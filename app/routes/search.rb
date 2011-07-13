c862233b04608aed8ea385f2889f044e18ddcfcb
Ben Darlow <github@kapowaz.net>
2011-07-13 08:14:07 +0100

    Fixed issue with missing @page ivar on search results page

--- app/routes/search.rb
+++ app/routes/search.rb
@@ -1,15 +1,16 @@
 # encoding: utf-8
 class Wowbom < Sinatra::Application
   post "/" do
-    results = Item.from_query(params[:query]).all(:recipe.not => nil, :order => [:quality_id.desc, :name.asc])
+    @query = params[:query]
+    @page  = { :title => "wowbom — searching for “#{@query}”" }
+    results = Item.from_query(@query).all(:recipe.not => nil, :order => [:quality_id.desc, :name.asc])
     
     if results.length == 1
       # redirect to that item
       redirect "/item/#{results.first.id}", 301
     elsif results.length > 1
-      redirect "/search/#{uri_escape params[:query].downcase.strip}", 301
+      redirect "/search/#{uri_escape @query.downcase.strip}", 301
     else
-      # TODO: error page 
       erb :notfound
     end
   end
